10267133785706d0456ec600537fed45eace6ad0
Charles Zhang <charleszhangmb@gmail.com>
2021-09-16 15:29:25 +0200

    Support refactoring

--- src/views/Support.vue
+++ src/views/Support.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="support">
     <div id="support-banner">
-      <h1>Donate to the Amplification Project</h1>
-      <p>
+      <p id="support-header">Donate to the Amplification Project</p>
+      <p id="support-banner-text">
         All gifts to The Amplification Project are meaningful, no matter how big
         or small. 100% of your donation goes directly to the mission of The
         Amplification Project.
@@ -12,9 +12,12 @@
         of The Amplification Project's website.
         <br />
         <br />
-        <span>
+        <span id="support-banner-text-small">
           The Amplification Project is in affiliation with
-          <a href="https://saveourplanet.org/" target="_blank"
+          <a
+            id="support-banner-link"
+            href="https://saveourplanet.org/"
+            target="_blank"
             >Social and Environmental Entrepreneurs (SEE)</a
           >, a non-profit public charity exempt from federal income tax under
           Section 501(c)(3) of the Internal Revenue Code. Donations to The
@@ -23,87 +26,105 @@
         </span>
       </p>
       <div id="support-nav">
-        <button id="donate-online-button" @click="setOnlinePage">
+        <button class="support-nav-button" @click="handlePageNav(0)">
           Donate Online
         </button>
-        <button id="donate-check-button" @click="setCheckPage">
+        <button class="support-nav-button" @click="handlePageNav(1)">
           Donate by Check
         </button>
       </div>
     </div>
     <div id="support-content">
-      <div id="support-form">
-        <form v-if="!altPage" @submit.prevent="handleSubmit">
-          <p>
-            <span>Make a Donation:</span>
-            <br />
-            <br />
-            Would you like to make a monthly or a one-time donation?
-            <span class="asterisk">*</span>
-          </p>
-          <div class="dono-buttons">
-            <button class="freq-buttons dono-button" @click="handleButton(0)">
-              Monthly
-            </button>
-            <button class="freq-buttons dono-button" @click="handleButton(1)">
-              One-Time
-            </button>
-          </div>
-          <p>
-            Choose an amount:
-            <span class="asterisk">*</span>
-          </p>
-          <div class="dono-buttons">
-            <button @click="handleButton(2)">$5</button>
-            <button @click="handleButton(3)">$10</button>
-            <button @click="handleButton(4)">$25</button>
-            <button @click="handleButton(5)">$100</button>
-            <input type="number" placeholder="Custom" v-model="res.donoAmnt" />
-          </div>
-          <p><span>Please tell us about yourself:</span></p>
-          <div id="personal-info">
-            <div class="personal-info-field">
-              <div class="personal-info-label">
-                <p>First Name: <span class="asterisk">*</span></p>
-              </div>
-              <input v-model="res.firstName" />
-            </div>
-            <div class="personal-info-field">
-              <div class="personal-info-label">
-                <p>Last Name: <span class="asterisk">*</span></p>
-              </div>
-              <input v-model="res.lastName" />
-            </div>
-            <div class="personal-info-field">
-              <div class="personal-info-label">
-                <p>Email: <span class="asterisk">*</span></p>
-              </div>
-              <input v-model="res.email" />
-            </div>
-          </div>
-          <input type="submit" value="Continue" />
-        </form>
-        <div v-else>
-          <p>
-            Please make checks payable to Social and Environmental Entrepreneurs
-            (SEE) with The Amplification Project written in the memo section.
-            Checks can be mailed to:
-            <br />
-            <br />
-            Social and Environmental Entrepreneurs
-            <br />
-            23532 Calabasas Road Suite A
-            <br />
-            Calabasas, CA 91302
-            <br />
-            Tel: 818-225-9150
-            <br />
-            <br />
-            <span
-              >Thank you for your donation to The Amplification Project!</span
-            >
-          </p>
+      <form id="support-form" v-if="!altPage" @submit.prevent="handleSubmit">
+        <p class="support-form-text">
+          <strong>Make a Donation:</strong>
+          <br />
+          <br />
+          Would you like to make a monthly or a one-time donation?
+          <span class="asterisk">*</span>
+        </p>
+        <div class="dono-buttons">
+          <button
+            class="freq-buttons dono-button"
+            @click="handleDonoButton(0)"
+          >
+            Monthly
+          </button>
+          <button
+            class="freq-buttons dono-button"
+            @click="handleDonoButton(1)"
+          >
+            One-Time
+          </button>
         </div>
+        <p class="support-form-text">
+          Choose an amount:
+          <span class="asterisk">*</span>
+        </p>
+        <div class="dono-buttons">
+          <button class="dono-button" @click="handleDonoButton(2)">
+            $5
+          </button>
+          <button class="dono-button" @click="handleDonoButton(3)">
+            $10
+          </button>
+          <button class="dono-button" @click="handleDonoButton(4)">
+            $25
+          </button>
+          <button class="dono-button" @click="handleDonoButton(5)">
+            $100
+          </button>
+          <input
+            class="dono-button"
+            type="number"
+            placeholder="Custom"
+            v-model="res.donoAmnt"
+            @click="handleDonoButton(6)"
+          />
+        </div>
+        <strong class="support-form-text"
+          >Please tell us about yourself:</strong
+        >
+        <div id="personal-info">
+          <div class="personal-info-field">
+            <label for="support-first-name">
+              First Name: <span class="asterisk">*</span>
+            </label>
+            <input id="support-first-name" v-model="res.firstName" />
+          </div>
+          <div class="personal-info-field">
+            <label for="support-last-name">
+              Last Name: <span class="asterisk">*</span>
+            </label>
+            <input id="support-last-name" v-model="res.lastName" />
+          </div>
+          <div class="personal-info-field">
+            <label for="support-email">
+              Email: <span class="asterisk">*</span>
+            </label>
+            <input id="support-email" v-model="res.email" />
+          </div>
+        </div>
+        <input id="dono-submit-button" type="submit" value="Continue" />
+      </form>
+      <div id="support-check" v-else>
+        <p class="support-text">
+          Please make checks payable to Social and Environmental Entrepreneurs
+          (SEE) with The Amplification Project written in the memo section.
+          Checks can be mailed to:
+          <br />
+          <br />
+          Social and Environmental Entrepreneurs
+          <br />
+          23532 Calabasas Road Suite A
+          <br />
+          Calabasas, CA 91302
+          <br />
+          Tel: 818-225-9150
+          <br />
+          <br />
+          <span>Thank you for your donation to The Amplification Project!</span>
+        </p>
       </div>
     </div>
   </div>
@@ -114,7 +135,7 @@ export default {
   name: "Support",
   data() {
     return {
-      altPage: false,
+      altPage: null,
       res: {
         donoFreq: null,
         donoAmnt: "",
@@ -126,42 +147,31 @@ export default {
   },
   methods: {
     // Navigation functions
-    setOnlinePage: function () {
-      this.altPage = false;
+    handlePageNav: function (ind) {
+      // Set page state
+      this.altPage = ind === 1;
       // Set underline
-      document.getElementById("donate-online-button").style.borderBottom =
-        "5px solid white";
-      document.getElementById("donate-check-button").style.borderBottom =
-        "none";
-    },
-    setCheckPage: function () {
-      this.altPage = true;
-      // Set underline
-      document.getElementById("donate-online-button").style.borderBottom =
-        "none";
-      document.getElementById("donate-check-button").style.borderBottom =
-        "5px solid white";
-    },
-    // Custom dono function
-    openCustomAmountModal: function () {
-      alert("This feature is being implemented still");
+      let arr = document.getElementsByClassName("support-nav-button");
+      for (let i = 0; i < arr.length; i++)
+        if (i === ind) arr[i].classList.add("nav-active");
+        else arr[i].classList.remove("nav-active");
     },
     // Button styling function
-    handleButton: function (i) {
+    handleDonoButton: function (ind) {
       let arr = document.getElementsByClassName("dono-button");
-      if (i < 2) {
-        for (let j = 0; j < 2; j++) {
-          arr[j].style.background = i === j ? "#298A7E" : "none";
-          arr[j].style.color = i === j ? "white" : "black";
-          arr[j].style.border = i === j ? "none" : "1px solid black";
+      // Style frequency buttons
+      if (ind < 2) {
+        for (let i = 0; i < 2; i++) {
+          if (i === ind) arr[i].classList.add("button-active");
+          else arr[i].classList.remove("button-active");
         }
         // FIX ME
         this.res.donoFreq = true;
+        // Style amount buttons
       } else {
-        for (let j = 2; j < 7; j++) {
-          arr[j].style.background = i === j ? "#298A7E" : "none";
-          arr[j].style.color = i === j ? "white" : "black";
-          arr[j].style.border = i === j ? "none" : "1px solid black";
+        for (let i = 2; i < 7; i++) {
+          if (i === ind) arr[i].classList.add("button-active");
+          else arr[i].classList.remove("button-active");
         }
         // FIX ME
         this.res.donoAmnt = true;
@@ -171,6 +181,10 @@ export default {
     handleSubmit() {
       console.log(this.res);
     },
+  },
+  mounted() {
+    // Set initial page styling
+    this.handlePageNav(0);
   },
 };
 </script>
@@ -192,7 +206,7 @@ export default {
     padding: calc(clamp(2.5rem, 1.5rem + 4vw, 5rem))
       calc(clamp(2rem, -1.2rem + 12.8vw, 10rem));
 
-    h1 {
+    #support-header {
       // Centering
       text-align: center;
       // Typography
@@ -201,19 +215,19 @@ export default {
       margin-bottom: 3rem;
     }
 
-    p {
+    #support-banner-text {
       // Typography
       font-family: $alt-font;
       font-size: calc(clamp(1rem, 0.8rem + 0.8vw, 1.5rem));
       // Spacing
       margin-bottom: calc(clamp(3rem, 1.8rem + 4.8vw, 6rem));
 
-      span {
+      #support-banner-text-small {
         // Typography
         font-size: calc(clamp(0.9rem, 0.76rem + 0.56vw, 1.25rem));
 
-        a {
-          // Remove default a tag styling
+        #support-banner-link {
+          // Remove default styling
           color: white;
         }
       }
@@ -223,13 +237,9 @@ export default {
       // Flexbox for layout
       display: flex;
       justify-content: space-evenly;
+      align-items: flex-start;
 
-      #donate-online-button {
-        // Default state of page
-        border-bottom: 5px solid white;
-      }
-
-      button {
+      .support-nav-button {
         // Remove default button styling
         border: none;
         background: transparent;
@@ -241,6 +251,11 @@ export default {
         font-family: $main-font;
         // Spacing the underline
         padding-bottom: 16px;
+      }
+
+      .nav-active {
+        // Underline
+        border-bottom: 5px solid white;
       }
     }
   }
@@ -261,11 +276,6 @@ export default {
       // Size border box
       padding: 3rem clamp(2rem, -1.2rem + 12.8vw, 5rem);
 
-      span {
-        // Bold selected lines
-        font-weight: $bold;
-      }
-
       .asterisk {
         // Asterisk color
         color: darken(red, 10);
@@ -276,17 +286,17 @@ export default {
         display: flex;
         justify-content: space-between;
         // Spacing
-        margin-top: 1rem;
-        margin-bottom: 3rem;
+        margin-top: 16px;
+        margin-bottom: 48px;
 
-        button,
-        input {
+        .dono-button {
           // Typography
           font-family: $alt-font;
           font-size: 1.2rem;
+          color: black;
           // Button sizing
           width: 17.5%;
-          padding: 1rem 0;
+          padding: 16px 0;
           // Remove default button styling
           background: transparent;
           // Shape button
@@ -296,11 +306,13 @@ export default {
           text-align: center;
           // Hide arrows
           -moz-appearance: textfield;
-        }
-
-        button {
           // Clickable
           cursor: pointer;
+        }
+
+        .freq-buttons {
+          // Sizing
+          width: 45%;
         }
 
         input::-webkit-outer-spin-button,
@@ -310,9 +322,16 @@ export default {
           margin: 0;
         }
 
-        .freq-buttons {
-          // Sizing
-          width: 45%;
+        .button-active {
+          // Button styling
+          border: none;
+          background: $accent-teal;
+          color: white;
+
+          &::placeholder {
+            // Restyle placeholder text
+            color: white;
+          }
         }
       }
 
@@ -322,25 +341,20 @@ export default {
         justify-content: space-between;
         flex-wrap: wrap;
         // Spacing
-        margin-top: 1rem;
+        margin-top: 16px;
 
         .personal-info-field {
           // Sizing
           width: max(300px, 45%);
-          margin-bottom: 2rem;
-
-          .personal-info-label {
-            // Flexbox for layout
-            display: flex;
-          }
+          margin-bottom: 32px;
 
           input {
             // Typography
             font-family: $alt-font;
             font-size: 1.2rem;
             // Spacing
-            margin-top: 0.5rem;
-            padding: 0.5rem;
+            margin-top: 8px;
+            padding: 8px;
             // Border styling
             border: none;
             border-bottom: 1px solid grey;
@@ -355,7 +369,7 @@ export default {
         }
       }
 
-      input[type="submit"] {
+      #dono-submit-button {
         // Button styling
         background: $accent-teal;
         border: none;
@@ -373,30 +387,37 @@ export default {
         display: block;
       }
     }
+
+    #support-check {
+      // Typography
+      font-family: $alt-font;
+      font-size: 1.2rem;
+      // Make border box
+      border: 1px solid $accent-teal;
+      // Size border box
+      padding: 3rem clamp(2rem, -1.2rem + 12.8vw, 5rem);
+      max-width: 70%;
+    }
   }
 }
 
 // Sticky hover
 @media (hover: hover) {
-  button {
-    // Smooth animation
-    transition: transform 0.2s ease;
+  #support > #support-content > #support-form {
+    .dono-button {
+      // Smooth animation
+      transition: transform 0.2s ease;
 
-    &:hover {
-      // Animate
-      transform: scale(1.05);
+      &:hover {
+        // Animate
+        transform: scale(1.05);
+      }
     }
-  }
 
-  #support {
-    #support-content {
-      #support-form {
-        input[type="submit"] {
-          &:hover {
-            // Animate
-            background: $accent-light-teal;
-          }
-        }
+    #dono-submit-button {
+      &:hover {
+        // Animate
+        background: $accent-light-teal;
       }
     }
   }
